c96ef12ea0a129b5b9798c9a9eb01fe84dad573e
tahsin <u1704102@student.cuet.ac.bd>
2024-09-17 01:29:06 +0600

    uploaded a text file to the server

---
+++ client.mjs
@@ -0,0 +1,17 @@
+import net from 'net';
+import fs from 'node:fs/promises';
+
+const socket = net.createConnection({host: "127.0.0.1", port: 4080},async ()=>{
+    const filePath = "./text.txt";
+    const fileHandle = await fs.open(filePath,"r");
+    const fileStream = fileHandle.createReadStream();
+
+    fileStream.on("data", (data)=> {
+        socket.write(data);
+    });
+
+    fileStream.on("end",()=>{
+        console.log("Connection Ended");
+        socket.end();
+    })
+});
---
+++ server.mjs
@@ -0,0 +1,19 @@
+import net from 'net';
+import fs from 'node:fs/promises';
+
+const server = net.createServer(()=>{
+
+});
+
+server.on('connection', (socket)=>{
+    console.log("new connection");
+    socket.on('data', async (data) => {
+        const fileHandle = await fs.open('./storage/test.txt',"w");
+        const fileStream = fileHandle.createWriteStream();
+        fileStream.write(data);
+    })
+});
+
+server.listen(4080, "127.0.0.1", ()=>{
+    console.log("Upload Server running at", server.address());
+})
