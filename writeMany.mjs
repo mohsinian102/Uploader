45073fa15a00f3d62c76e4f45d2c33ad761b102d
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-09-19 12:48:25 +0600

    created 5gb dummy data file

--- writeMany.mjs
+++ writeMany.mjs
@@ -7,9 +7,9 @@ import * as fs from 'node:fs/promises';
     const stream  = fileHandle.createWriteStream();
     let i = 0;
     const writeMany = () => {
-        while(i < 1e6) {
+        while(i < 500e6) {
             const buff = Buffer.from(` ${i}`, "utf-8");
-            if(i==999999) return stream.end(buff);
+            if(i==499999999) return stream.end(buff);
             if(!stream.write(buff)) break;
             i++;
         }
