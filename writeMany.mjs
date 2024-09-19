4e24056b707a5960dc29bfb927231833eac8e979
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-09-19 10:43:58 +0600

    writeMany should generate fake big size file

---
+++ writeMany.mjs
@@ -0,0 +1,29 @@
+import * as fs from 'node:fs/promises';
+
+(async () => {
+    console.time("writeMany");
+    const fileHandle = await fs.open("upload.txt","w");
+
+    const stream  = fileHandle.createWriteStream();
+    let i = 0;
+    const writeMany = () => {
+        while(i < 1e6) {
+            const buff = Buffer.from(` ${i}`, "utf-8");
+            if(i==999999) return stream.end(buff);
+            if(!stream.write(buff)) break;
+            i++;
+        }
+    }
+
+
+    stream.on('drain',()=> {
+        writeMany();
+    })
+
+    writeMany();
+
+    stream.on('finish', ()=> {
+        console.timeEnd("writeMany");
+        fileHandle.close();
+    })
+}) ();
